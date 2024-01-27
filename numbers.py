ea76e60fa329daacc2bc5bdd1471e0632f2846c7
JuanMauwu <juanmavc111@gmail.com>
2024-01-27 12:18:20 -0500

    ejercicios numeros

---
+++ numbers.py
@@ -0,0 +1,38 @@
+#ejercicio 1
+entero = 1        #tipo entero
+flotante = 1.5    #tipo float(decimal)
+complejo = 1j   #tipo complejo
+
+a = 1
+b = 5473573457547345747  #a, b y c enteros
+c = -235235
+
+a = 1.55
+b = 1.0                  #a, b y c decimales(float)
+b = -234.232353
+
+a = 20E50                #numeros float pero en notacion cientifica (20*(10^50))
+b = 15e-20                   
+c = -56e100
+
+a = 12j
+b = 5+75j                #complex
+c = -100j
+
+#ejercicio 2
+x = 1    # int
+y = 2.8  # float    
+z = 1j   # complex
+
+x = float(x)
+y = int(y)
+z = complex(x)  # Aquí se cambia de x a z
+
+print(x)
+print(y)
+print(z)
+
+#ejercicio 3
+import random
+a = random.randrange(1, 100)  #para un numero random
+print(a)
