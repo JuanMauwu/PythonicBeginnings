beb89e47d6c7d55d1a9f182fd7bc41ff5af3385a
JuanMauwu <juanmavc111@gmail.com>
2024-01-27 15:39:12 -0500

    ejercicios de casting

---
+++ casting.py
@@ -0,0 +1,23 @@
+#ejercicio 1
+x = int("100")
+y = int(2.546534)
+print(x)
+print(y)
+
+#ejercicio 3
+x = float(1)
+y = float("1234124")
+z = float("2132.124")
+a = float(2.5)
+print(x)
+print(y)
+print(z)
+print(a)
+
+#ejercicio 4
+x = str(1213)
+y = str("123123")
+z = str(213.214214)
+print(x)
+print(y)
+print(z)
