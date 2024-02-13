8e920358df885672dec95fc44289818391904387
JuanMauwu <juanmavc111@gmail.com>
2024-02-12 20:28:11 -0500

    ejercicios tuplas

--- sets.py
+++ sets.py
@@ -7,7 +7,7 @@ print(new_set)
 #{'banana', 'watermelon', 'cherry'}
 
 #ejercicio 3
-set = {'banana', 'watermelon', 'cherry', True, 1} #True y 1 en los sets valen lo mismo. Se puede ver que el set al imprimirlo aparece en otro orden
+set = {'banana', 'watermelon', 'cherry', True, 1} #True y 1 en los sets valen lo mismo. Se puede ver que el set al imprimirlo aparece EN DESORDEN
 print(set)                                        #Igual el 0 y el False.
 #{'cherry', True, 'banana', 'watermelon'}
 
@@ -20,7 +20,7 @@ len(new_set)
 variado = {"hello", 1, 1.5, False, True} #permite contener varios tipos de datos.
 
 #ejercicio 6
-variado = {"hello", 1, 1.5, False, True} #funcion tupe() devuelve set.
+variado = {"hello", 1, 1.5, False, True} #funcion type() devuelve set.
 type(variado)
 # <class 'set'>
 
@@ -53,10 +53,48 @@ numbers.add(1000)           #usar metodo add() para agragar elementos
 
 #ejercicio 11
 numbers = {1, 2, 3, 4}          
-new_set = ["banana", "cherry", "watermelon"]    #metodo update() sirve para agragar elementos de otro iterable.
+new_set = ["banana", "cherry", "watermelon"]    #metodo update() sirve para agregar elementos de otro iterable.
 numbers.update(new_set)
 #{1, 2, 3, 4, 'banana', 'cherry', 'watermelon'}
 
+#ejercicio 12       #metodo intersection() para crear una nueva tupla con los valores presentes en x y Y.
+x = {1,2,3}
+y = {1,4,5,6}
+z = x.intersection(y)
+# {1}
+
+#ejercicio 13
+x = {"apple", "banana", "lemon"}    # A x se le dan los elementos de que no esten presentes tanto en x como en y.
+y = {"google", "kiwi", "apple"}
+x.symmetric_difference_update(y)
+print(x) #{'lemon', 'kiwi', 'google', 'banana'}
+
+#ejercicio 14
+x = {"apple", "banana", "cherry"}  # metodo symmetric_difference(), pero este sirve para crear otra variable.
+y = {"google", "microsoft", "apple"}
+
+z = x.symmetric_difference(y)
+#{'google', 'cherry', 'banana', 'microsoft'}
+
 #----------------------------
 
-#ejercicio 12
+#add()	         Adds an element to the set
+#clear()	     Removes all the elements from the set
+#copy()	         Returns a copy of the set
+#difference()	 Returns a set containing the difference between two or more sets
+#difference_update()	Removes the items in this set that are also included in another, specified set
+#discard()	     Remove the specified item
+#intersection()	 Returns a set, that is the intersection of two other sets
+#intersection_update()	Removes the items in this set that are not present in other, specified set(s)
+#isdisjoint()	 Returns whether two sets have a intersection or not
+#issubset()	     Returns whether another set contains this set or not
+#issuperset()	 Returns whether this set contains another set or not
+#pop()	         Removes an element from the set
+#remove()	     Removes the specified element
+#symmetric_difference()	Returns a set with the symmetric differences of two sets
+#symmetric_difference_update()	inserts the symmetric differences from this set and another
+#union()	     Return a set containing the union of sets
+#update()	     Update the set with the union of this set and others
+
+
+
