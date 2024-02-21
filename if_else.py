0b70d3f2fc1525853451f065556d0c9440db52c1
JuanMauwu <juanmavc111@gmail.com>
2024-02-20 20:51:49 -0500

    ejercicios if

--- if_else.py
+++ if_else.py
@@ -15,6 +15,16 @@ if a < b:
 elif a > b:
     print("'a' es mayor a b.")
 
+#extra
+a = 100
+b = 50
+if a > b:
+    print("1")
+elif a >= b:
+    print("2")
+# 1
+# en este caso ambas sentencias son correctas, pero solo se aplica la primera.
+
 #ejercicio 3   #else, si ninguna de las anteriores se cunmple entra al else y acaba.
 a = 50
 b = 50
@@ -57,7 +67,7 @@ b = 2
 print("x") if a == b else print("y") if a < b else print("z") 
 # z
 
-#ejercicio 8   unsado el operador logico and y operadores ternarios. 
+#ejercicio 8   unsado palabra clave and. Todo debe ser verdad para que arroje verdadero. 
 a = 100
 b = 50
 c = 50
@@ -72,3 +82,46 @@ if b == c and a < b:
 else:
     print("y")
 # y
+    
+#ejercicio 9  palabra clave or, si una es verdadera todo da verdadero
+a = 100
+b = 50
+c = 50
+
+if a < b or a > c:
+    print("nice")
+# nice
+    
+#ejercicio 10   la palabra clave "not" niega el resultado de la sentenca. True => False y viceversa
+a = 100
+b = 50
+#esta sentencia da False, pero con "not" se convierte en True y se ejecuta la instruccion.
+if not b > a:        
+    print("nice")
+# nice
+
+# ejercicio 11          if anidado.
+a = 100
+
+if a > 10:
+    print("mayor a 10")
+    if a > 50:
+        print("mayor a 50")
+    else:
+        print("menor a 50")
+#mayor a 10
+#mayor a 50
+
+#ejercicio 12   si por alguna razon queremos un if sin contenido agragamos pass.
+a = 100
+b = 50
+
+if a > b:
+    pass
+
+a = 100
+b = 50
+if a > b:
+    print("1")
+elif a >= b:
+    print("2")
